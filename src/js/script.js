797e92a4f5c317cfc2e5ff51e7150c4607a04e38
Victor <lyagkin13@gmail.com>
2023-03-21 13:32:08 +0300

    start tabs project

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item"), // список табов
+    tabsContent = document.querySelectorAll(".tabcontent"), // список контента
+    tabsParent = document.querySelector(".tabheader__items"); // родитель табов для делегирования
+
+  function hideTabContent() {
+    // функция которыя скрывает табы и класс активности у табов со страницы
+    tabsContent.forEach((item) => {
+      item.classList.add("hideTabContent");
+      item.classList.remove("showTabContent", "fade");
+
+      tabs.forEach((item) => {
+        item.classList.remove("tabheader__item_active");
+      });
+    });
+  }
+
+  function showTabContent(i = 0) {
+    // функция которая, принимает как аргумент индекс таба из списка и по нему показывает этот таб, по умолчанию принимает аргумент первого таба
+    tabsContent[i].classList.add("showTabContent", "fade");
+    tabsContent[i].classList.remove("hideTabContent");
+
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent(); // скрываем все табы и классы активности у них
+  showTabContent(); // показываем первый таб из списка с классом активности
+
+  tabsParent.addEventListener("click", (e) => {
+    // вешаем обработчик события клика на родителя табов, проверяем содержит ли элеменат на который кликаем класс табов, если да перебираем все табы и проверям какой из них равен тому на который кликнули, тогда скрываем все и показываем этот
+    const target = e.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (item === target) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
